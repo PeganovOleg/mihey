75dca94987974df0242264eeae61eb5f48f009da
PeganovOleg <59733896+PeganovOleg@users.noreply.github.com>
2021-06-27 11:27:31 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,112 @@
+<?php
+
+// Настройки Шаблона
+$theme = 'slate'; //выберете тему оформления (все названия можете увидеть на сайте в верхнем правом углу)
+$themeswitcher = 'on'; // переключатель тем оформления вкл (on) или выкл (off)
+$smooth_scroll = 'on'; // медленная прокрутка вкл (on) или выкл (off)
+$show_categories = true; // показывать категории вкл (true) или выкл (false)
+$show_locations = true; // показывать местоположение вкл (true) или выкл (false)
+
+
+// Реклама & Партнёрки
+
+$careerjet_publisher_id = "f75d46fdf225795bd3aef73a89a780d02"; // получите свой ID здесь http://www.careerjet.com/partners/
+
+$adsenseads = ''; // любой код , например тизерки, google adsense или яндекс метрика итп
+
+
+//Переключать язык установленный по умолчанию
+$default_country = 'off'; // вкл (on) или выкл (off)
+
+
+// Ваш код Google аналитики 
+$statistics_code = "UA-XXXXXXX";
+
+
+// Ваш профиль в addthis.com для продвижения сайта
+$addthis = "ra-597b27689c5b5921";
+
+
+// Установка языка по умолчанию
+$locale = 'ru-RU'; // язык по умолчанию
+$language_switcher = 'on'; //переключатель языков  вкл (on) или выкл (off)
+
+
+// Настройки поиска
+$limit_listings = "10"; // количество объявлений при использовании поиска
+$default_keyword = ""; // установите ключевое слово(вакансию) по умолчанию
+$default_location = ""; // установите местоположение по умолчанию
+
+
+// GLOBAL LOCALE CONFIG
+
+$_locales = array(
+'VN' => 'vi-VN',
+'NL' => 'nl-NL',
+'US' => 'en-US',
+'UK' => 'uk-UK',
+'TR' => 'tr-TR',
+'CZ' => 'cs-CZ',
+'DK' => 'da-DK',
+'AT' => 'de-AT',
+'CH' => 'de-CH',
+'DE' => 'de-DE',
+'AE' => 'en-AE',
+'AU' => 'en-AU',
+'CA' => 'en-CA',
+'CN' => 'en-CN',
+'HK' => 'en-HK',
+'IE' => 'en-IE',
+'IN' => 'en-IN',
+'MY' => 'en-MY',
+'NZ' => 'en-NZ',
+'OM' => 'en-OM',
+'PH' => 'en-PH',
+'PK' => 'en-PK',
+'QA' => 'en-QA',
+'SG' => 'en-SG',
+'GB' => 'en-GB',
+'ZA' => 'en-ZA',
+'TW' => 'en-TW',
+'VN' => 'en-VN',
+'AR' => 'es-AR',
+'BO' => 'es-BO',
+'CL' => 'es-CL',
+'CR' => 'es-CR',
+'DO' => 'es-DO',
+'EC' => 'es-EC',
+'ES' => 'es-ES',
+'GT' => 'es-GT',
+'MX' => 'es-MX',
+'PA' => 'es-PA',
+'PE' => 'es-PE',
+'PR' => 'es-PR',
+'PY' => 'es-PY',
+'UY' => 'es-UY',
+'VE' => 'es-VE',
+'FI' => 'fi-FI',
+'CA' => 'fr-CA',
+'BE' => 'fr-BE',
+'CH' => 'fr-CH',
+'FR' => 'fr-FR',
+'LU' => 'fr-LU',
+'MA' => 'fr-MA',
+'HU' => 'hu-HU',
+'IT' => 'it-IT',
+'JP' => 'ja-JP',
+'KR' => 'ko-KR',
+'BE' => 'nl-BE',
+'NO' => 'no-NO',
+'PL' => 'pl-PL',
+'PT' => 'pt-PT',
+'BR' => 'pt-BR',
+'RU' => 'ru-RU',
+'UA' => 'ru-UA',
+'SE' => 'sv-SE',
+'SK' => 'sk-SK',
+'UA' => 'uk-UA',
+'CN' => 'zh-CN'
+);
+
+// END
+?>
